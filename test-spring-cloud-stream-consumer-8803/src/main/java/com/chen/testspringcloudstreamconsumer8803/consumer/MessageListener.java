a211bea8b3119f18b956ce33c32b3304b13c17ba
陈乐 <111>
2020-11-19 18:30:45 +0800

    mq

--- test-spring-cloud-stream-consumer-8803/src/main/java/com/chen/testspringcloudstreamconsumer8803/consumer/MessageListener.java
+++ test-spring-cloud-stream-consumer-8803/src/main/java/com/chen/testspringcloudstreamconsumer8803/consumer/MessageListener.java
@@ -9,7 +9,7 @@ import org.springframework.messaging.SubscribableChannel;
 @EnableBinding(MySink.class)
 public class MessageListener {
 
-	//监听通道消息,接收到消息后处理业务逻辑如果有异常，会重新执行方法（默认最多执行3次）
+	//监听通道消息,接收到消息后处理业务逻辑如果有异常，会重新调此方法（默认最多执行3次）
 	@StreamListener("myInput")//这里配置的是要监听的消息通道（消费通道）
 	public void input(Message message) {
 		System.out.println("获取到消息: "+message.getPayload());
