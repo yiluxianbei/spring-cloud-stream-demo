edeb4bcbb6f8e4050d1e2f07b6b75b3a1c186726
陈乐 <123>
2021-10-31 09:25:40 +0800

    默认的

--- test-spring-cloud-stream-consumer-8802/src/main/java/com/chen/testspringcloudstreamconsumer/listener/MessageListener.java
+++ test-spring-cloud-stream-consumer-8802/src/main/java/com/chen/testspringcloudstreamconsumer/listener/MessageListener.java
@@ -2,16 +2,20 @@ package com.chen.testspringcloudstreamconsumer.listener;
 
 import com.chen.testspringcloudstreamconsumer.channel.MySink;
 import com.chen.testspringcloudstreamconsumer.pojo.MyPayLoad;
+import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.support.GenericMessage;
+
+import java.io.IOException;
 
 @EnableBinding(MySink.class)
 public class MessageListener {
 
 	//监听通道消息,接收到消息后处理业务逻辑如果有异常，会重新执行方法（默认最多执行3次）
 	@StreamListener(MySink.MY_CONSUMER)//这里配置的是要监听的消息通道（消费通道）
-	public void input(GenericMessage<MyPayLoad> message) {
+	public void input(GenericMessage<MyPayLoad> message, @Header(AmqpHeaders.DELIVERY_TAG) Long deliveryTag) throws IOException {
 		System.out.println("获取到消息: "+message.getPayload());
 	}
 	//监听通道消息(测试延时消息)
--- test-spring-cloud-stream-consumer-8803/src/main/java/com/chen/testspringcloudstreamconsumer/listener/MessageListener.java
+++ test-spring-cloud-stream-consumer-8803/src/main/java/com/chen/testspringcloudstreamconsumer/listener/MessageListener.java
@@ -2,12 +2,10 @@ package com.chen.testspringcloudstreamconsumer.listener;
 
 import com.chen.testspringcloudstreamconsumer.channel.MySink;
 import com.chen.testspringcloudstreamconsumer.pojo.MyPayLoad;
-import org.springframework.amqp.rabbit.annotation.Exchange;
-import org.springframework.amqp.rabbit.annotation.Queue;
-import org.springframework.amqp.rabbit.annotation.QueueBinding;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.support.GenericMessage;
 
 @EnableBinding(MySink.class)
@@ -15,7 +13,7 @@ public class MessageListener {
 
 	//监听通道消息,接收到消息后处理业务逻辑如果有异常，会重新调此方法（默认最多执行3次）
 	@StreamListener(MySink.MY_CONSUMER)//这里配置的是要监听的消息通道（消费通道）
-	public void input(GenericMessage<MyPayLoad> message) {
+	public void input(GenericMessage<MyPayLoad> message, @Header(AmqpHeaders.DELIVERY_TAG) Long deliveryTag) {
 		System.out.println("获取到消息: "+message.getPayload());
 		throw new RuntimeException("123");
 	}
